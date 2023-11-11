62da140454dbc72e8f413c8af58c34711ef9632e
Shaun Saker <sakershaun@gmail.com>
2023-11-12 09:19:32 +1000

    fix(website): custom layout tweaks

--- packages/app/src/pages/dashboard/components/selectedEmojiRenderer/SelectedEmojiRenderer.tsx
+++ packages/app/src/pages/dashboard/components/selectedEmojiRenderer/SelectedEmojiRenderer.tsx
@@ -1,59 +1,58 @@
 import { ClipboardDocumentIcon, XMarkIcon } from '@heroicons/react/24/solid'
 import { Button, Card, ParagraphText, TitleText } from 'components'
-import React from 'react'
+import React, { ComponentPropsWithoutRef } from 'react'
 import toast from 'react-hot-toast'
+import { twMerge } from 'tailwind-merge'
 
 import { useSelectedEmojis } from '@/emojis/hooks/useSelectedEmojis'
 
 import { EmojiButton } from '../emojiButton/EmojiButton'
 
-export const SelectedEmojiRenderer = () => {
+type Props = ComponentPropsWithoutRef<'div'>
+
+export const SelectedEmojiRenderer = ({ className = '', ...props }: Props) => {
   const [selectedEmojis, setSelectedEmojis] = useSelectedEmojis()
 
+  const actionsDisabled = !selectedEmojis.length
+
   return (
-    <div className="h-full overflow-hidden p-8">
-      <Card className="flex h-full flex-col gap-y-4">
-        <div className="flex justify-end gap-x-4">
-          <Button
-            className="h-10 w-10 rounded-full p-0"
-            variant="secondary"
-            disabled={!selectedEmojis.length}
-            onClick={() => {
-              navigator.clipboard.writeText(selectedEmojis.join(''))
-              toast.success('Copied to clipboard')
-            }}
+    <Card className={twMerge('flex flex-col gap-y-4', className)} {...props}>
+      <div className="flex justify-end gap-x-4">
+        <Button
+          className="h-10 w-10 rounded-full p-0"
+          variant="secondary"
+          disabled={actionsDisabled}
+          onClick={() => {
+            navigator.clipboard.writeText(selectedEmojis.join(''))
+            toast.success('Copied to clipboard')
+          }}
+        >
+          <ClipboardDocumentIcon className="h-5 w-5" />
+        </Button>
+
+        <Button className="h-10 w-10 rounded-full p-0" disabled={actionsDisabled} onClick={() => setSelectedEmojis([])}>
+          <XMarkIcon className="h-5 w-5" />
+        </Button>
+      </div>
+
+      <div className="flex h-full flex-wrap justify-center overflow-y-auto">
+        {!selectedEmojis.length && (
+          <div className="flex flex-col gap-y-4 text-center">
+            <TitleText>Hey Lala and friends 👋🏻</TitleText>
+
+            <ParagraphText>Click on an emoji to get started 🤠</ParagraphText>
+          </div>
+        )}
+
+        {selectedEmojis.map((emoji, index) => (
+          <EmojiButton
+            key={index}
+            onClick={() => setSelectedEmojis(prev => prev.filter((_, prevIndex) => prevIndex !== index))}
           >
-            <ClipboardDocumentIcon className="h-5 w-5" />
-          </Button>
-
-          <Button
-            className="h-10 w-10 rounded-full p-0"
-            disabled={!selectedEmojis.length}
-            onClick={() => setSelectedEmojis([])}
-          >
-            <XMarkIcon className="h-5 w-5" />
-          </Button>
-        </div>
-
-        <div className="flex h-full flex-wrap justify-center overflow-y-auto">
-          {!selectedEmojis.length && (
-            <div className="flex flex-col gap-y-4 text-center">
-              <TitleText>Hey Lala and friends 👋🏻</TitleText>
-
-              <ParagraphText>Click on an emoji to get started 🤠</ParagraphText>
-            </div>
-          )}
-
-          {selectedEmojis.map((emoji, index) => (
-            <EmojiButton
-              key={index}
-              onClick={() => setSelectedEmojis(prev => prev.filter((_, prevIndex) => prevIndex !== index))}
-            >
-              {emoji}
-            </EmojiButton>
-          ))}
-        </div>
-      </Card>
-    </div>
+            {emoji}
+          </EmojiButton>
+        ))}
+      </div>
+    </Card>
   )
 }
--- packages/app/src/pages/dashboard/index.tsx
+++ packages/app/src/pages/dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Header } from '@/components/header/Header'
+import { PageLayout } from '@/components/pageLayout/PageLayout'
 import { EmojiPicker } from '@/pages/dashboard/components/emojiPicker/EmojiPicker'
 
 import { SelectedEmojiRenderer } from './components/selectedEmojiRenderer/SelectedEmojiRenderer'
@@ -10,9 +11,11 @@ export const Dashboard = () => {
     <>
       <Header />
 
-      <SelectedEmojiRenderer />
+      <PageLayout className="flex flex-col">
+        <SelectedEmojiRenderer className="mb-8 flex-1" />
 
-      <EmojiPicker />
+        <EmojiPicker className="-mb-8 flex-[2] lg:-mb-12" />
+      </PageLayout>
     </>
   )
 }

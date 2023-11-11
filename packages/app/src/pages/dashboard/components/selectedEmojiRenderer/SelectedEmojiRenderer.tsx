39d2d8dcb309398ddbb2fde653903a7c2ed9ae11
Shaun Saker <sakershaun@gmail.com>
2023-11-12 07:33:32 +1000

    feat(app): custom add copy button

--- packages/app/src/pages/dashboard/components/selectedEmojiRenderer/SelectedEmojiRenderer.tsx
+++ packages/app/src/pages/dashboard/components/selectedEmojiRenderer/SelectedEmojiRenderer.tsx
@@ -1,6 +1,7 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ClipboardDocumentIcon, XMarkIcon } from '@heroicons/react/24/solid'
 import { Button, Card } from 'components'
 import React from 'react'
+import toast from 'react-hot-toast'
 
 import { useSelectedEmojis } from '@/emojis/hooks/useSelectedEmojis'
 
@@ -12,8 +13,24 @@ export const SelectedEmojiRenderer = () => {
   return (
     <div className="m-8 h-full overflow-hidden">
       <Card className="flex h-full flex-col gap-y-4">
-        <div className="flex justify-end">
-          <Button className="h-10 w-10 rounded-full p-0" onClick={() => setSelectedEmojis([])}>
+        <div className="flex justify-end gap-x-4">
+          <Button
+            className="h-10 w-10 rounded-full p-0"
+            variant="secondary"
+            disabled={!selectedEmojis.length}
+            onClick={() => {
+              navigator.clipboard.writeText(selectedEmojis.join(''))
+              toast.success('Copied to clipboard')
+            }}
+          >
+            <ClipboardDocumentIcon className="h-5 w-5" />
+          </Button>
+
+          <Button
+            className="h-10 w-10 rounded-full p-0"
+            disabled={!selectedEmojis.length}
+            onClick={() => setSelectedEmojis([])}
+          >
             <XMarkIcon className="h-5 w-5" />
           </Button>
         </div>

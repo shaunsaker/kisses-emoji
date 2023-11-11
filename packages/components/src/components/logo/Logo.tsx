39324fa2b591e5ac88338c892e026e40f537e131
Shaun Saker <sakershaun@gmail.com>
2023-11-12 07:19:22 +1000

    feat(app): custom setup

--- packages/components/src/components/logo/Logo.tsx
+++ packages/components/src/components/logo/Logo.tsx
@@ -4,15 +4,30 @@ import { twMerge } from 'tailwind-merge'
 import { app } from '../../../../config'
 import { HeadingText } from '../headingText/HeadingText'
 import { Logomark } from '../logomark/Logomark'
+import { SmallText } from '../smallText/SmallText'
 
-type Props = ComponentPropsWithoutRef<'div'>
+type Props = ComponentPropsWithoutRef<'div'> & {
+  variant?: 'default' | 'inverted'
+}
 
-export const Logo = ({ className, ...props }: Props) => {
+export const Logo = ({ className, variant = 'default', ...props }: Props) => {
   return (
     <div className={twMerge('flex items-center gap-x-2', className)} {...props}>
-      <Logomark className={className} />
+      <Logomark className={className} variant={variant} />
 
-      <HeadingText className={twMerge('whitespace-nowrap', className)}>{app.name}</HeadingText>
+      <div className={variant === 'inverted' ? 'text-theme-content-inverted dark:text-theme-content-inverted' : ''}>
+        <HeadingText
+          className={twMerge(
+            'whitespace-nowrap',
+
+            className,
+          )}
+        >
+          {app.name}
+        </HeadingText>
+
+        <SmallText className="font-display -mt-1">For Lala</SmallText>
+      </div>
     </div>
   )
 }

6e7b305580bd4693ee197c218c5a051042f95c37
Shaun Saker <sakershaun@gmail.com>
2023-11-12 07:25:47 +1000

    fix(app): custom remove irrelevant tests

--- packages/app/src/components/mainLayout/MainLayout.test.tsx
+++ packages/app/src/components/mainLayout/MainLayout.test.tsx
@@ -1,9 +1,8 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import React from 'react'
 import { describe, expect, it, vi } from 'vitest'
 
 import { useHasActiveSubscription } from '@/billing/hooks/useHasActiveSubscription'
-import { routes } from '@/router/routes'
 import { cleanUpAfterEach } from '@/test/cleanUpAfterEach'
 import { MockAppProvider } from '@/test/MockAppProvider'
 
@@ -43,43 +42,5 @@ describe('MainLayout', () => {
     )
 
     expect(getDashboardButton()).toBeInTheDocument()
-  })
-
-  it('disables the Dashboard button if loading the active subscription', () => {
-    mocks.useHasActiveSubscription.mockReturnValue({ data: false, isLoading: false })
-
-    render(
-      <MockAppProvider>
-        <MainLayout />
-      </MockAppProvider>,
-    )
-
-    expect(getDashboardButton()).toBeDisabled()
-  })
-
-  it('disables the Dashboard button if the user does not have an active subscription', () => {
-    mocks.useHasActiveSubscription.mockReturnValue({ data: false, isLoading: false })
-
-    render(
-      <MockAppProvider>
-        <MainLayout />
-      </MockAppProvider>,
-    )
-
-    expect(getDashboardButton()).toBeDisabled()
-  })
-
-  it('navigates to the Settings page when the Settings button is clicked', () => {
-    mocks.useHasActiveSubscription.mockReturnValue({ data: true, isLoading: false })
-
-    render(
-      <MockAppProvider>
-        <MainLayout />
-      </MockAppProvider>,
-    )
-
-    fireEvent.click(screen.getByRole('button', { name: 'Settings' }))
-
-    expect(mocks.navigate).toHaveBeenCalledWith(routes.settings)
   })
 })

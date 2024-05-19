a9773934a190505895850dbfd9b4150edc183bcc
Shaun Saker <sakershaun@gmail.com>
2024-05-19 15:45:02 +1000

    feat(app): list scroll to bottom and better instructions

--- packages/app/src/components/pageLayout/PageLayout.tsx
+++ packages/app/src/components/pageLayout/PageLayout.tsx
@@ -8,7 +8,7 @@ export const PageLayout = ({ className = '', ...props }: Props) => {
   return (
     <div
       className={twMerge(
-        'bg-theme-background dark:bg-dark-theme-background h-full overflow-y-auto px-6 py-8 lg:px-8 lg:py-12',
+        'bg-theme-background dark:bg-dark-theme-background h-full overflow-y-auto pt-6 lg:pt-8',
         className,
       )}
       {...props}
--- packages/app/src/pages/dashboard/components/selectedEmojiRenderer/SelectedEmojiRenderer.tsx
+++ packages/app/src/pages/dashboard/components/selectedEmojiRenderer/SelectedEmojiRenderer.tsx
@@ -1,6 +1,6 @@
 import { ClipboardDocumentIcon, XMarkIcon } from '@heroicons/react/24/solid'
 import { Button, ParagraphText, TitleText } from 'components'
-import React, { ComponentPropsWithoutRef } from 'react'
+import React, { ComponentPropsWithoutRef, useEffect, useRef } from 'react'
 import toast from 'react-hot-toast'
 import { twMerge } from 'tailwind-merge'
 
@@ -11,13 +11,23 @@ import { EmojiButton } from '../emojiButton/EmojiButton'
 type Props = ComponentPropsWithoutRef<'div'>
 
 export const SelectedEmojiRenderer = ({ className = '', ...props }: Props) => {
+  const selectedEmojisListRef = useRef<HTMLDivElement>(null)
+
   const [selectedEmojis, setSelectedEmojis] = useSelectedEmojis()
 
   const actionsDisabled = !selectedEmojis.length
 
+  // when the selected emojis change, scroll to the end of the list
+  useEffect(() => {
+    selectedEmojisListRef.current?.scrollTo({
+      top: selectedEmojisListRef.current.scrollHeight,
+      behavior: 'smooth',
+    })
+  }, [selectedEmojis])
+
   return (
-    <div className={twMerge('flex flex-col gap-y-8', className)} {...props}>
-      <div className="flex justify-end gap-x-4">
+    <div className={twMerge('flex flex-1 flex-col gap-y-4 overflow-hidden', className)} {...props}>
+      <div className="flex justify-end gap-x-4 px-8">
         <Button
           className="h-10 w-10 rounded-full p-0"
           variant="secondary"
@@ -35,23 +45,30 @@ export const SelectedEmojiRenderer = ({ className = '', ...props }: Props) => {
         </Button>
       </div>
 
-      <div className="flex h-full flex-wrap justify-center overflow-y-auto">
-        {!selectedEmojis.length && (
-          <div className="flex flex-col gap-y-4 text-center">
-            <TitleText>Hey Lala and friends 👋🏻</TitleText>
+      <div ref={selectedEmojisListRef} className="flex flex-1 flex-wrap justify-center overflow-y-auto">
+        {!selectedEmojis.length ? (
+          <div className="flex max-w-md flex-col gap-y-4 text-center">
+            <TitleText>Hey Lala and friends!</TitleText>
 
-            <ParagraphText>Click on an emoji to get started 🤠</ParagraphText>
+            <ParagraphText>
+              Welcome to Kisses Emoji where you can learn how to move the cursor, scroll, click and clear with this fun
+              emoji game. Click on an emoji to get started 🤠
+            </ParagraphText>
           </div>
+        ) : (
+          selectedEmojis.map((emoji, index) => (
+            <EmojiButton
+              key={index}
+              onClick={() => setSelectedEmojis(prev => prev.filter((_, prevIndex) => prevIndex !== index))}
+              style={{
+                // triggers hardware acceleration necessary to fix rendering issues when scrolling quickly
+                transform: 'translateZ(0)',
+              }}
+            >
+              {emoji}
+            </EmojiButton>
+          ))
         )}
-
-        {selectedEmojis.map((emoji, index) => (
-          <EmojiButton
-            key={index}
-            onClick={() => setSelectedEmojis(prev => prev.filter((_, prevIndex) => prevIndex !== index))}
-          >
-            {emoji}
-          </EmojiButton>
-        ))}
       </div>
     </div>
   )
